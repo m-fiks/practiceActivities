0af2951d4e9114509b1e7797593bc7e5b0b1a1c0
m-fiks <fiksd005@umn.edu>
2018-09-11 22:09:51 -0500

    chunksarray

---
+++ chunks.js
@@ -0,0 +1,15 @@
+//split into two arrays 
+//size of array is the number given
+function chunkArrayInGroups(arr, size) {
+    // Break it up.
+    let results = [];
+
+    while (arr.length) {
+        results.push(arr.splice(0, size))
+    }
+
+    console.log(results);
+
+  }
+  
+  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2)
