ff5477a4e775c36f430f201562379dcafa4e3c89
m-fiks <fiksd005@umn.edu>
2018-10-10 12:40:38 -0500

    name

--- name.js
+++ name.js
@@ -1,15 +1,17 @@
 function whatIsInAName(collection, source) {
     // What's in a name?
-    var arr = [];
     // Only change code below this line
-    collection.forEach((elem) => {
-       if(elem.indexOf(source) > -1){
-           console.log(elem)
-       }
+    let srcKeys = Object.keys(source);
+       
+    return collection.filter((obj) => {
+        for(let i=0; i<srcKeys.length; i++) {
+            if(!obj.hasOwnProperty(srcKeys[i]) || obj[srcKeys[i]] !== source[srcKeys[i]] ){
+                return false
+            }
+        }
+        return true;
     })
-    
     // Only change code above this line
-    return arr;
   }
   
   whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, 
