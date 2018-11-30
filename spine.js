f9258107f83a548813eb584934ad036f615f9447
m-fiks <fiksd005@umn.edu>
2018-11-30 10:41:26 -0800

    new laptop werkin

--- spine.js
+++ spine.js
@@ -1,6 +1,7 @@
 function spinalCase(str) {
     // "It's such a fine line between stupid, and clever."
     // --David St. Hubbins
+    console.log('hi')
     return str.toLowerCase().split(' ').join('-');
   }
   
